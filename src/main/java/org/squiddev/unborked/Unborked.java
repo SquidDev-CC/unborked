ad189ee238517ddcd7768e82323f615053d71d42
SquidDev <bonzoweb@hotmail.co.uk>
2016-11-07 00:35:39 +0000

    Fix disk colors not being set

--- src/main/java/org/squiddev/unborked/Unborked.java
+++ src/main/java/org/squiddev/unborked/Unborked.java
@@ -1,6 +1,14 @@
 package org.squiddev.unborked;
 
+import dan200.computercraft.ComputerCraft;
+import dan200.computercraft.shared.media.items.ItemDiskLegacy;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.color.IItemColor;
+import net.minecraft.item.ItemStack;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 
 /**
  * Empty class to ensure textures get loaded correctly
@@ -11,4 +19,29 @@ import net.minecraftforge.fml.common.Mod;
 	dependencies = "required-after:ComputerCraft@[1.80pr0,);after:CCTurtle"
 )
 public class Unborked {
+	@Mod.EventHandler
+	@SideOnly(Side.CLIENT)
+	public void init(FMLInitializationEvent event) {
+		setupColors();
+	}
+
+	@SideOnly(Side.CLIENT)
+	private void setupColors() {
+		Minecraft.getMinecraft().getItemColors().registerItemColorHandler(new DiskColorHandler(ComputerCraft.Items.disk), ComputerCraft.Items.disk);
+		Minecraft.getMinecraft().getItemColors().registerItemColorHandler(new DiskColorHandler(ComputerCraft.Items.diskExpanded), ComputerCraft.Items.diskExpanded);
+	}
+
+	@SideOnly(Side.CLIENT)
+	private static class DiskColorHandler implements IItemColor {
+		private final ItemDiskLegacy disk;
+
+		private DiskColorHandler(ItemDiskLegacy disk) {
+			this.disk = disk;
+		}
+
+		@Override
+		public int getColorFromItemstack(ItemStack stack, int layer) {
+			return layer == 0 ? 0xFFFFFF : disk.getColor(stack);
+		}
+	}
 }
